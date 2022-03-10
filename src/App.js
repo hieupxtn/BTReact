45a412e3a784c1415af8531f383a2f15ff349a80
your name <hieupxtn@gmail.com>
2022-03-10 14:33:57 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Members, TransferMember } from './Component/React';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Members/>
+      <TransferMember/>
+    </div>
+  );
+}
+
+export default App;
